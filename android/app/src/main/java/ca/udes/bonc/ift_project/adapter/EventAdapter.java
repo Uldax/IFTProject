74b7e31a655a3ee14c4dcd2e94ad121ae1c67a3d
CharlyBong <CharlyBong@users.noreply.github.com>
2015-12-12 12:47:11 -0500

    Ajout Adapter NEED : un objet event et des données

---
+++ android/app/src/main/java/ca/udes/bonc/ift_project/adapter/EventAdapter.java
@@ -0,0 +1,91 @@
+package ca.udes.bonc.ift_project.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+import ca.udes.bonc.ift_project.R;
+
+/**
+ * Created by cbongiorno on 12/12/2015.
+ */
+public class EventAdapter extends ArrayAdapter<Object> {
+    Context context;
+    int layoutResourceId;
+    private List<Object> data;
+
+    public EventAdapter(Context context, int layoutResourceId, List<Object> data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.data = data;
+
+        Log.i("EventAdapter", "Created View");
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        EventHolder holder = null;
+        Object event = data.get(position);
+
+        if(row == null)
+        {
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new EventHolder();
+            holder.title = (TextView)row.findViewById(R.id.title);
+            holder.date = (TextView)row.findViewById(R.id.title);
+            holder.place = (TextView)row.findViewById(R.id.place);
+            holder.image = (ImageView)row.findViewById(R.id.image);
+
+            row.setTag(holder);
+        }
+        else
+        {
+            holder = (EventHolder)row.getTag();
+        }
+
+        /*holder.title.setText(event.getName());
+        holder.date.setText(event.getDate());
+        holder.place.setText(event.getPlace());
+
+        switch (event.getCategorie()){
+            case ... :
+                holder.image.setImageDrawable(R.drawable.run);
+            break;
+            default:
+                holder.image.setImageDrawable(R.drawable.run);
+        }*/
+
+        return row;
+    }
+
+    static class EventHolder
+    {
+        TextView title;
+        TextView date;
+        TextView place;
+        ImageView image;
+    }
+
+    @Override
+    public void notifyDataSetChanged(){
+        super.notifyDataSetChanged();
+        Log.i("EventAdapter","Update View");
+    }
+
+    public List<Object> getData(){
+        return data;
+    }
+}
--- android/app/src/main/java/ca/udes/bonc/ift_project/fragment/MapFragment.java
+++ android/app/src/main/java/ca/udes/bonc/ift_project/fragment/MapFragment.java
@@ -2,10 +2,12 @@ package ca.udes.bonc.ift_project.fragment;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -18,6 +20,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,7 +28,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Collections;
+import java.util.List;
+
 import ca.udes.bonc.ift_project.R;
+import ca.udes.bonc.ift_project.adapter.EventAdapter;
+import ca.udes.bonc.ift_project.communication.QueryEventService;
+import ca.udes.bonc.ift_project.communication.QueryIntentService;
+import ca.udes.bonc.ift_project.communication.RestApiResultReceiver;
 import ca.udes.bonc.ift_project.utils.AlertDialogManager;
 import ca.udes.bonc.ift_project.utils.GPSTracker;
 
@@ -41,11 +51,14 @@ import ca.udes.bonc.ift_project.utils.GPSTracker;
 public class MapFragment extends Fragment implements
         GoogleMap.OnMyLocationButtonClickListener,
         ActivityCompat.OnRequestPermissionsResultCallback,
-        OnMapReadyCallback {
+        OnMapReadyCallback,
+        RestApiResultReceiver.Receiver {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static RestApiResultReceiver mReceiver;
+    private String TAG = "MapFragment";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,6 +73,8 @@ public class MapFragment extends Fragment implements
     private View myView;
     private static GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private AlertDialogManager alert = new AlertDialogManager();
+    private EventAdapter adapter;
+    private ProgressBar progressBar;
 
     private SupportMapFragment mSupportMapFragment;
 
@@ -81,7 +96,6 @@ public class MapFragment extends Fragment implements
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
-
         return fragment;
     }
 
@@ -96,6 +110,8 @@ public class MapFragment extends Fragment implements
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mReceiver = new RestApiResultReceiver(new Handler());
+        mReceiver.setReceiver(this);
 
        gps = new GPSTracker(getActivity()){
             @Override
@@ -128,6 +144,7 @@ public class MapFragment extends Fragment implements
         this.fabList = (FloatingActionButton) myView.findViewById(R.id.fabList);
         this.fabLoc = (FloatingActionButton) myView.findViewById(R.id.fabLoc);
         this.listMap = (ListView) myView.findViewById(R.id.listMap);
+        this.progressBar = (ProgressBar) myView.findViewById(R.id.progressBar);
 
         mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mSupportMapFragment == null) {
@@ -159,7 +176,7 @@ public class MapFragment extends Fragment implements
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.e("maps", " Call to on map ready");
+        Log.e("maps", "Call to on map ready");
         mMap = googleMap;
         mMap.setOnMyLocationButtonClickListener(this);
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
@@ -168,6 +185,7 @@ public class MapFragment extends Fragment implements
         } else if (mMap != null) {
             mMap.setMyLocationEnabled(true);
         }
+        QueryEventService.startActionGetMarkers(getContext(), mReceiver, "10.2", "23");
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -211,5 +229,46 @@ public class MapFragment extends Fragment implements
 
     protected void localisateMe(){
         Snackbar.make(this.myView, "Localisation TODO", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        switch (resultCode) {
+            case QueryIntentService.STATUS_RUNNING:
+                Log.i(TAG, "Runing status");
+                this.progressBar.setVisibility(View.VISIBLE);
+                //show progress
+                break;
+            case QueryIntentService.STATUS_FINISHED:
+                String results = resultData.getString("results");
+                this.progressBar.setVisibility(View.GONE);
+                Log.i(TAG, "result =");
+                Log.i(TAG, results);
+                // TODO : update listMap (How ? I don't now !)
+                updateEventList(null);
+                break;
+            case QueryIntentService.STATUS_ERROR:
+                //todo handl error
+                this.progressBar.setVisibility(View.GONE);
+                String error = resultData.getString(Intent.EXTRA_TEXT);
+                Log.d(TAG, "error = " + error);
+                break;
+        }
+    }
+
+    public void updateEventList(List<Object> listEvent){
+        listMap = (ListView) myView.findViewById(R.id.listMap);
+
+        if((listEvent==null)||(listEvent.size()==0)){
+            return;
+        }
+
+        /*Collections.sort(listEvent, new Comparator<Event>() {
+            public int compare(Event o1, Event o2) {
+                //TODO : sort by ... somethings
+            }
+        });*/
+
+        adapter = new EventAdapter(getContext(),R.layout.adapter_event, listEvent);
+        listMap.setAdapter(adapter);
     }
 }
