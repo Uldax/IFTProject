abf56413cd96dca494f3b187d45971cf744106af
julienasp <julien.aspirot@usherbrooke.ca>
2015-12-14 17:14:09 -0500

    first commit

---
+++ android/app/src/main/java/ca/udes/bonc/ift_project/dataObject/Team.java
@@ -0,0 +1,55 @@
+package ca.udes.bonc.ift_project.dataObject;
+
+
+import java.util.HashMap;
+import java.util.ArrayList;
+/**
+ * Created by juasp on 14/12/2015.
+ */
+public class Team {
+
+    private String id;
+    private String name;
+    private ArrayList<String> participantsList = new ArrayList<String>();
+    private String image;
+
+    public Team(String id, int name,String participantsList, String image ){
+
+        this.id = id;
+        this.name= name;
+        this.participantsList = participantsList;
+        this.image = startNameOfImage;
+
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getParticipantsList() {
+        return participantsList;
+    }
+
+    public void setParticipantsList(ArrayList<String> participantsList) {
+        this.participantsList = participantsList;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
