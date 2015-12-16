18724c7ec39dde5a7df887264c205cf5685fcef6
CharlyBong <CharlyBong@users.noreply.github.com>
2015-12-15 23:18:32 -0500

    Button pour accéder à la page de management des teams

--- android/app/src/main/java/ca/udes/bonc/ift_project/fragment/MapFragment.java
+++ android/app/src/main/java/ca/udes/bonc/ift_project/fragment/MapFragment.java
@@ -203,7 +203,7 @@ public class MapFragment extends Fragment implements
             @Override
             public void onCameraChange(CameraPosition cameraPosition) {
                 Log.i("maps Cam change", "Camera : " + cameraPosition.target.latitude + " - " + cameraPosition.target.longitude);
-                QueryEventService.startActionGetMarkersByRadius(getContext(), mReceiver, String.valueOf(cameraPosition.target.longitude), String.valueOf(cameraPosition.target.latitude), 2);
+                QueryEventService.startActionGetMarkersByRadius(getContext(), mReceiver, String.valueOf(cameraPosition.target.longitude), String.valueOf(cameraPosition.target.latitude), 20);
             }
         });
     }
